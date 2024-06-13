890f53da4d2d7a88e24b7b2e0906fa9af0a65069
githubscientist <sathish.datascientist@gmail.com>
2024-06-13 19:50:50 +0530

    class component & props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-function App() {
-  return (
-    <h1>Hello, World!</h1>
-  )
+import { Component } from "react";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Notes</h1>
+        <ul>
+          {this.props.notes.map(note => <li key={note.id}>{note.content}</li>)}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
