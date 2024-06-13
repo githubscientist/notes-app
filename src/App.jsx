fe3f2559758162a1c9c2e20a66c046ab068cdad2
githubscientist <sathish.datascientist@gmail.com>
2024-06-13 19:52:34 +0530

    multiple class components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import { Component } from "react";
 
+class Note extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <li>{this.props.note.content}</li>
+    )
+  }
+}
+
 class App extends Component {
 
   constructor(props) {
@@ -11,7 +23,12 @@ class App extends Component {
       <div>
         <h1>Notes</h1>
         <ul>
-          {this.props.notes.map(note => <li key={note.id}>{note.content}</li>)}
+          {this.props.notes.map(note =>
+            <Note
+              note={note} 
+              key={note.id}
+            />
+          )}
         </ul>
       </div>
     );
