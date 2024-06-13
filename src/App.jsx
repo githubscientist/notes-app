6caeed99c27e55b7de7d40163a6d2c5db1e448f6
githubscientist <sathish.datascientist@gmail.com>
2024-06-13 19:42:15 +0530

    moving component to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-function Note({note}) {
-  return <li key={note.id}>{note.content}</li>;
-}
+import Note from './components/Note';
 
 function App(props) {
-
-  // const notesList = [];
-
-  // for (let i = 0; i < props.notes.length; i++){
-  //   notesList.push(<li>{ props.notes[i].content }</li>);
-  // }
-
   return (
     <div>
       <h1>Notes</h1>
       <ul>
           {
           props.notes.map(note => 
-            <Note 
+            <Note
+              key={note.id}  
               note={note}
             />
           )
